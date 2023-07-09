95730347487f56439d6085bbda3ca7c834426822
Darren Seubert <dpseubert15@gmail.com>
2023-07-09 01:53:03 -0500

    Added cheat sheet, comments, and fixed servo bug

--- DrinkTable.ino
+++ DrinkTable.ino
@@ -1,9 +1,10 @@
 #include "arduino_secrets.h"
 #include <SparkFun_APDS9960.h> // SparkFun APDS9960 RGB and Gesture Sensor - Version: Latest
-#include <Wire.h> // Enable IC2 Communication
+#include <Wire.h> // Enables IC2 Communication
 #include <ESP32Servo.h> // ESP32Servo - Version: Latest
 #include "thingProperties.h"
 
+// Declarations
 enum RGBColor {
   Red, Green, Blue, Yellow, Cyan, Purple, White, Black
 };
@@ -42,9 +43,7 @@ boolean recentPour = false;
 double shotPumpTime = 6.0;
 double mixerPumpTime = 8.5;
 
-/**
-
-*/
+/// @brief Function called once on boot of program
 void setup() {
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
@@ -67,6 +66,7 @@ void setup() {
   pinMode(elevatorMotorPin2, OUTPUT);
 
   servo.attach(servoPin);
+  servoSetPosition(0.0); // TODO Tune Rotation Percentage
 
   Serial.begin(9600); // Initialize serial and wait for port to open:
   delay(1500); // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
@@ -91,10 +91,9 @@ void setup() {
   prox.enableProximitySensor(false); // Start running the prox-9960 proximity sensor (no interrupts)
 }
 
-/**
-
-*/
+/// @brief Function called repeatly during execution of program
 void loop() {
+  // Check if connected to Arduino IoT Cloud
   if (!ArduinoCloud.connected()) {
     Serial.println("Connecting to Arduino IoT Cloud...");
     firstRun = true;
@@ -109,23 +108,24 @@ void loop() {
   ArduinoCloud.update();
 }
 
-/**
-
-*/
+/// @brief Function called when a change in Drink Selection is detected
 void onDrinkSelectionChange()  {
+  // Check if the change is due to connecting to the internet
   if (firstRun) {
     firstRun = false;
     connectionBlink(true);
     return;
   }
   
+  // Retrieve the table code and reset it back to 0
   int tableCode = drinkSelection.getBrightness();
   drinkSelection.setBrightness(0);
   Serial.println("-------");
   Serial.print("Table Code: ");
   Serial.println(tableCode);
 
-  if (tableCode == 99) { // Clears Error State of Table
+  // Clear Error State of Table
+  if (tableCode == 99) {
     errorState = false;
     errorBlinkCode = 0;
     Serial.println("Error State Cleared");
@@ -133,13 +133,15 @@ void onDrinkSelectionChange()  {
     return;
   }
 
-  if (errorState) { // Checks if Table is in Error State
+  // Checks if Table is in Error State
+  if (errorState) {
     Serial.println("Error: Table in Error State, Please Correct and Clear the Error");
     errorBlink(errorBlinkCode, true);
     return;
   }
 
-  if (tableCode == 97) { // Homes Table to Top Limit
+  // Homes Table to Top Limit
+  if (tableCode == 97) {
     Serial.println("Homing to Top Limit...");
     if (!elevatorMove(true)) { // Move Up until Top Limit
       Serial.println("Error: Elevator Movement Timed Out");
@@ -149,7 +151,8 @@ void onDrinkSelectionChange()  {
     return;
   }
 
-  if (tableCode == 98) { // Homes Table to Bottom Limit
+  // Homes Table to Bottom Limit
+  if (tableCode == 98) {
     Serial.println("Homing to Bottom Limit...");
     if (!elevatorMove(false)) { // Move Up until Top Limit
       Serial.println("Error: Elevator Movement Timed Out");
@@ -159,52 +162,59 @@ void onDrinkSelectionChange()  {
     return;
   }
 
-  if (recentPour) { // Checks if the Glass has been Replaced
+  // Checks if the Glass has been Replaced
+  if (recentPour) {
     Serial.println("Error: Table has Detected a Recent Pour, Replace the Glass with a New One");
     errorBlink(4, false);
     return;
   }
 
-  if (!isGlassPresent()) { // Checks if a Glass is Present
+  // Checks if a Glass is Present
+  if (!isGlassPresent()) {
     Serial.println("Error: Glass is not Present");
     errorBlink(2, false);
     return;
   }
 
+  // Detects a Non-Valid Drink is Selected
   double pump1Time, pump2Time, pump3Time, pump4Time = 0.0;
-  if (!getPumpTimes(tableCode, pump1Time, pump2Time, pump3Time, pump4Time)) { // Detects a Non-Valid Drink is Selected
+  if (!getPumpTimes(tableCode, pump1Time, pump2Time, pump3Time, pump4Time)) {
     Serial.println("Error: Invalid Drink Selection");
     errorBlink(3, false);
     return;
   }
-
   Serial.println("-Pump Times-");
   Serial.println(pump1Time);
   Serial.println(pump2Time);
   Serial.println(pump3Time);
   Serial.println(pump4Time);
 
-  // Drink Routine Starts Here:
+  // Drink Routine Starts Here
   if (!elevatorMove(false)) { // Move Elevator Down until Bottom Limit
     Serial.println("Error: Elevator Movement Timed Out");
     errorBlink(2, true);
     return;
   }
 
-  servoSetPosition(1.0); // Move Servo to Pouring Position
+  // Move Servo to Pouring Position
+  servoSetPosition(1.0); // TODO Tune Rotation Percentage
   delay(2000);
 
-  if (!pumpLiquid(pump1Time, pump2Time, pump3Time, pump4Time)) { // Pump Liquid
+  // Pump Liquid
+  if (!pumpLiquid(pump1Time, pump2Time, pump3Time, pump4Time)) {
     Serial.println("Error: Pump Liquid Failed");
+    servoSetPosition(0.0); // TODO Tune Rotation Percentage
     errorBlink(3, true);
     return;
   }
   delay(1000);
-
-  servoSetPosition(0.0); // Move Servo to Stored Position
+  
+  // Move Servo to Stored Position
+  servoSetPosition(0.0); // TODO Tune Rotation Percentage
   delay(2000);
 
-  if (!elevatorMove(true)) { // Move Elevator Up until Top Limit
+  // Move Elevator Up until Top Limit
+  if (!elevatorMove(true)) {
     Serial.println("Error: Elevator Movement Timed Out");
     errorBlink(2, true);
     return;
@@ -213,9 +223,8 @@ void onDrinkSelectionChange()  {
   setColor(Black);
 }
 
-/**
-   Returns True if a glass is present, else false
-*/
+/// @brief Function that determines if a glass is present
+/// @return True if a glass is present, false if not present or if there is an error reading the value
 boolean isGlassPresent() {
   uint8_t proxData;
   if (!prox.readProximity(proxData)) {
@@ -228,9 +237,8 @@ boolean isGlassPresent() {
   }
 }
 
-/**
-
-*/
+/// @brief Function that sets the servo's position given a percent of rotation
+/// @param percent A value between 0.0 and 1.0 to be converted into the servo's target rotation position in degrees
 void servoSetPosition(double percent) {
   if (percent < 0.0) {
     percent = 0.0;
@@ -241,38 +249,39 @@ void servoSetPosition(double percent) {
   servo.write(180 * percent);
 }
 
-/**
-   Returns True if timeout or wrap around did not occur
-   TODO Tune Timeouts
-*/
+/// @brief Function that moves the elevator until the correct limit switch is pressed with a timeout
+/// @param up Whether the desired movement of the elevator is up or down
+/// @return True if the movement did not timeout or time wrap around did not occur, else false
 boolean elevatorMove(boolean up) {
   unsigned long startTime = millis();
   if (up) { // Move Up
-    while (!digitalRead(topLimit) && (millis() - startTime) < 4000 && (millis() - startTime) >= 0) {
+    while (!digitalRead(topLimit) && (millis() - startTime) < 4000 && (millis() - startTime) >= 0) { // TODO Tune Timeouts
       motorSetSpeed(elevatorMotorPWM, elevatorMotorPin1, elevatorMotorPin2, 1.0);
     }
   } else { // Move Down
-    while (!digitalRead(bottomLimit) && (millis() - startTime) < 4000 && (millis() - startTime) >= 0) {
+    while (!digitalRead(bottomLimit) && (millis() - startTime) < 4000 && (millis() - startTime) >= 0) { // TODO Tune Timeouts
       motorSetSpeed(elevatorMotorPWM, elevatorMotorPin1, elevatorMotorPin2, -1.0);
     }
   }
   motorSetSpeed(elevatorMotorPWM, elevatorMotorPin1, elevatorMotorPin2, 0.0);
 
-  return ((millis() - startTime) < 3000 && (millis() - startTime) >= 0);
+  return ((millis() - startTime) < 4000 && (millis() - startTime) >= 0); // TODO Tune Timeouts
 }
 
-/**
-
-*/
-void motorSetSpeed(int pwmPin, int pin1, int pin2, double speed) {
-  if (speed < -1.0) {
-    speed = -1.0;
-  } else if (speed > 1.0) {
-    speed = 1.0;
+/// @brief Function that sets a motors speed and direction
+/// @param pwmPin The PWM pin that the motor controller is plugged into
+/// @param pin1 One of the pins that the motor controller is plugged into
+/// @param pin2 One of the pins that the motor controller is plugged into
+/// @param percentage A value between -1.0 and 1.0 that represents the percentage and direction for the motor to run 
+void motorSetSpeed(int pwmPin, int pin1, int pin2, double percentage) {
+  if (percentage < -1.0) {
+    percentage = -1.0;
+  } else if (percentage > 1.0) {
+    percentage = 1.0;
   }
 
-  int value = round(abs(255 * speed));
-  if (speed >= 0) {
+  int value = round(abs(255 * percentage));
+  if (percentage >= 0) {
     analogWrite(pwmPin, value);
     digitalWrite(pin1, HIGH);
     digitalWrite(pin2, LOW);
@@ -283,14 +292,13 @@ void motorSetSpeed(int pwmPin, int pin1, int pin2, double speed) {
   }
 }
 
-/**
-   Returns false if glass is not present during pump or time wrap around occurs
-*/
+/// @brief Function that pumps liquid given the desired duration
+/// @param pump1Time The desired pump time in seconds
+/// @param pump2Time The desired pump time in seconds
+/// @param pump3Time The desired pump time in seconds
+/// @param pump4Time The desired pump time in seconds
+/// @return True if the glass stays present for the duraction of the fucntion and time wrap did not occur, else false
 boolean pumpLiquid(double pump1Time, double pump2Time, double pump3Time, double pump4Time) {
-  if (!isGlassPresent()) {
-    return false;
-  }
-
   double maxPumpTime = max(max(pump1Time, pump2Time), max(pump3Time, pump4Time));
   double elapsedTime = 0.0;
   double startTime = millis() / 1000.0;
@@ -332,9 +340,9 @@ boolean pumpLiquid(double pump1Time, double pump2Time, double pump3Time, double 
   return (isGlassPresent() && elapsedTime >= 0.0);
 }
 
-/**
-
-*/
+/// @brief Function that sets the blink code and state of table and blinks LEDs
+/// @param times Number of times for the LEDs to blink
+/// @param state True if fatal error, false if warning error 
 void errorBlink(int times, boolean state) {
   errorBlinkCode = times;
   errorState = state;
@@ -354,9 +362,8 @@ void errorBlink(int times, boolean state) {
   }
 }
 
-/**
-
-*/
+/// @brief Function that blinks the LEDs to show the Arduino IoT Cloud connection state
+/// @param connected True if the table is connected, false if not connected
 void connectionBlink(boolean connected) {
   RGBColor color;
   int delayTime;
@@ -375,9 +382,8 @@ void connectionBlink(boolean connected) {
   delay(delayTime);
 }
 
-/**
-
-*/
+/// @brief Function that sets the color of the LEDs
+/// @param color The desired color for the LEDs to be set to
 void setColor(RGBColor color) {
   switch (color) {
     case Red:
@@ -428,14 +434,13 @@ void setColor(RGBColor color) {
   }
 }
 
-/**
-   Given drink code, set pump times and color
-
-   pump1 = Apple Brandy
-   pump2 = Cran-Grape Juice
-   pump3 = UV Blue
-   pump4 = Lemonade
-*/
+/// @brief Function that given the drink code will set the pump times and LED color
+/// @param drinkCode The numeric code of the drink
+/// @param pump1Time The time to be set for this pump
+/// @param pump2Time The time to be set for this pump
+/// @param pump3Time The time to be set for this pump
+/// @param pump4Time The time to be set for this pump
+/// @return True if a valid drink's pump times were calculated, else false 
 boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double &pump3Time, double &pump4Time) {
   boolean validDrink = false;
 
@@ -448,7 +453,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Green);
       validDrink = true;
       break;
-    case 2:  // Apple Brandy Straight Tall
+    case 2: // Apple Brandy Straight Tall
       pump1Time = shotPumpTime * 2; // Apple Brandy
       pump2Time = 0.0; // Cran-Grape
       pump3Time = 0.0; // UV Blue
@@ -456,7 +461,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Green);
       validDrink = true;
       break;
-    case 3:  // UV Blue Straight
+    case 3: // UV Blue Straight
       pump1Time = 0.0; // Apple Brandy
       pump2Time = 0.0; // Cran-Grape
       pump3Time = shotPumpTime; // UV Blue
@@ -464,7 +469,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Blue);
       validDrink = true;
       break;
-    case 4:  // UV Blue Straight Tall
+    case 4: // UV Blue Straight Tall
       pump1Time = 0.0; // Apple Brandy
       pump2Time = 0.0; // Cran-Grape
       pump3Time = shotPumpTime * 2; // UV Blue
@@ -472,7 +477,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Blue);
       validDrink = true;
       break;
-    case 5:  // UV Blue Cran-Grape
+    case 5: // UV Blue Cran-Grape
       pump1Time = 0.0; // Apple Brandy
       pump2Time = mixerPumpTime; // Cran-Grape
       pump3Time = shotPumpTime; // UV Blue
@@ -480,7 +485,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Purple);
       validDrink = true;
       break;
-    case 6:  // UV Blue Cran-Grape Tall
+    case 6: // UV Blue Cran-Grape Tall
       pump1Time = 0.0; // Apple Brandy
       pump2Time = mixerPumpTime; // Cran-Grape
       pump3Time = shotPumpTime * 2; // UV Blue
@@ -488,7 +493,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Purple);
       validDrink = true;
       break;
-    case 7:  // UV Blue Lemonade
+    case 7: // UV Blue Lemonade
       pump1Time = 0.0; // Apple Brandy
       pump2Time = 0.0; // Cran-Grape
       pump3Time = shotPumpTime; // UV Blue
@@ -496,7 +501,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Cyan);
       validDrink = true;
       break;
-    case 8:  // UV Blue Lemonade Tall
+    case 8: // UV Blue Lemonade Tall
       pump1Time = 0.0; // Apple Brandy
       pump2Time = 0.0; // Cran-Grape
       pump3Time = shotPumpTime; // UV Blue
@@ -504,7 +509,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Cyan);
       validDrink = true;
       break;
-    case 9:  // Cran-Grape
+    case 9: // Cran-Grape
       pump1Time = 0.0; // Apple Brandy
       pump2Time = mixerPumpTime; // Cran-Grape
       pump3Time = 0.0; // UV Blue
@@ -512,7 +517,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Red);
       validDrink = true;
       break;
-    case 10:  // Cran-Grape Tall
+    case 10: // Cran-Grape Tall
       pump1Time = 0.0; // Apple Brandy
       pump2Time = mixerPumpTime * 2; // Cran-Grape
       pump3Time = 0.0; // UV Blue
@@ -520,7 +525,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Red);
       validDrink = true;
       break;
-    case 11:  // Lemonade
+    case 11: // Lemonade
       pump1Time = 0.0; // Apple Brandy
       pump2Time = 0.0; // Cran-Grape
       pump3Time = 0.0; // UV Blue
@@ -528,7 +533,7 @@ boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double
       setColor(Yellow);
       validDrink = true;
       break;
-    case 12:  // Lemonade Tall
+    case 12: // Lemonade Tall
       pump1Time = 0.0; // Apple Brandy
       pump2Time = 0.0; // Cran-Grape
       pump3Time = 0.0; // UV Blue
