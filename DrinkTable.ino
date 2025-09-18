83559d751f5e98c9c34f10e55c4fa3105c02d6e8
Darren Seubert <dpseubert15@gmail.com>
2025-09-18 00:13:32 -0500

    Fixed all wrap around logic, clean up

--- DrinkTable.ino
+++ DrinkTable.ino
@@ -69,14 +69,11 @@ void setup() {
   pinMode(proxGround, OUTPUT);
 
   Serial.begin(9600); // Initialize serial and wait for port to open:
-  delay(1500); // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  delay(1500); // Delay briefly to allow a Serial Monitor to connect without blocking if none found
   initProperties(); // Defined in thingProperties.h
   ArduinoCloud.begin(ArduinoIoTPreferredConnection); // Connect to Arduino IoT Cloud
 
-  /*
-     The following function allows you to obtain more information related to the state of network and IoT Cloud connection and errors
-     the higher number the more granular information you’ll get. The default is 0 (only errors), maximum is 4.
-  */
+  // Gets network/IoT Cloud connection status and errors (0 = errors only, 4 = maximum detail).
   setDebugMessageLevel(2);
   ArduinoCloud.printDebugInfo();
 
@@ -234,7 +231,8 @@ void onDrinkSelectionChange()  {
 }
 
 /// @brief Function that determines if a glass is present
-/// @return True if a glass is present, false if not present or if there is an error reading the value
+/// @return True if a glass is present, false if not present or if there is an error reading the
+/// value
 boolean isGlassPresent() {
   uint8_t proxData;
   if (!prox.readProximity(proxData)) {
@@ -247,9 +245,10 @@ boolean isGlassPresent() {
   }
 }
 
-/// @brief Function that sets the servo's position given a percent of rotation. To reduce noise from constant
-/// minor corrections, the target position is adjusted by ±5 units after a short delay.
-/// @param percent A value between 0.0 and 1.0 with 0.0 being stored position and 1.0 being pouring position
+/// @brief Function that sets the servo's position given a percent of rotation. To reduce noise from
+/// constant minor corrections, the target position is adjusted by ±5 units after a short delay.
+/// @param percent A value between 0.0 and 1.0 with 0.0 being stored position and 1.0 being pouring
+/// position
 void servoSetPosition(double percent) {
   if (percent < 0.0) {
     percent = 0.0;
@@ -274,29 +273,29 @@ void servoSetPosition(double percent) {
 
 /// @brief Function that moves the elevator until the correct limit switch is pressed with a timeout
 /// @param up Whether the desired movement of the elevator is up or down
-/// @return True if the movement did not timeout or time wrap around did not occur, else false
+/// @return True if the movement did not timeout, else false
 boolean elevatorMove(boolean up) {
   unsigned long startTime = millis();
   const unsigned long timeout = 8000;
-  if (up) { // Move Up
-    while (!digitalRead(topLimit) && (millis() - startTime) < timeout) {
-      motorSetSpeed(elevatorMotorChannel, elevatorMotorPin1, elevatorMotorPin2, 1.0);
-    }
-  } else { // Move Down
-    while (!digitalRead(bottomLimit) && (millis() - startTime) < timeout) {
-      motorSetSpeed(elevatorMotorChannel, elevatorMotorPin1, elevatorMotorPin2, -1.0);
-    }
+
+  // Select direction and limit switch depending on up
+  double direction = up ? 1.0 : -1.0;
+  int limitPin = up ? topLimit : bottomLimit;
+
+  while (!digitalRead(limitPin) && (millis() - startTime) < timeout) {
+    motorSetSpeed(elevatorMotorChannel, elevatorMotorPin1, elevatorMotorPin2, direction);
   }
   motorSetSpeed(elevatorMotorChannel, elevatorMotorPin1, elevatorMotorPin2, 0.0);
 
-  return ((millis() - startTime) < timeout);
+  return (millis() - startTime) < timeout;
 }
 
 /// @brief Function that sets a motor's speed and direction
 /// @param motorChannel The channel that the motor controller is running on
 /// @param pin1 One of the pins that the motor controller is plugged into
 /// @param pin2 One of the pins that the motor controller is plugged into
-/// @param percentage A value between -1.0 and 1.0 that represents the percentage and direction for the motor to run 
+/// @param percentage A value between -1.0 and 1.0 that represents the percentage and direction
+/// for the motor to run
 void motorSetSpeed(int motorChannel, int pin1, int pin2, double percentage) {
   if (percentage < -1.0) {
     percentage = -1.0;
@@ -325,37 +324,23 @@ void motorSetSpeed(int motorChannel, int pin1, int pin2, double percentage) {
 /// @param pump2Time The desired pump time in seconds
 /// @param pump3Time The desired pump time in seconds
 /// @param pump4Time The desired pump time in seconds
-/// @return True if the glass stays present for the duration of the function and time wrap did not occur, else false
+/// @return True if the glass stays present for the duration of the function, else false
 boolean pumpLiquid(double pump1Time, double pump2Time, double pump3Time, double pump4Time) {
-  double maxPumpTime = max(max(pump1Time, pump2Time), max(pump3Time, pump4Time));
-  double elapsedTime = 0.0;
-  double startTime = millis() / 1000.0;
+  unsigned long pump1Dur = (unsigned long)(pump1Time * 1000.0);
+  unsigned long pump2Dur = (unsigned long)(pump2Time * 1000.0);
+  unsigned long pump3Dur = (unsigned long)(pump3Time * 1000.0);
+  unsigned long pump4Dur = (unsigned long)(pump4Time * 1000.0);
 
-  while (maxPumpTime > elapsedTime && isGlassPresent() && elapsedTime >= 0.0) {
-    elapsedTime = (millis() / 1000.0) - startTime;
-    if (elapsedTime < pump1Time) {
-      digitalWrite(pump1, HIGH);
-    } else {
-      digitalWrite(pump1, LOW);
-    }
+  unsigned long maxPumpTime = max(max(pump1Dur, pump2Dur), max(pump3Dur, pump4Dur));
+  unsigned long elapsedTime = 0;
+  unsigned long startTime = millis();
 
-    if (elapsedTime < pump2Time) {
-      digitalWrite(pump2, HIGH);
-    } else {
-      digitalWrite(pump2, LOW);
-    }
-
-    if (elapsedTime < pump3Time) {
-      digitalWrite(pump3, HIGH);
-    } else {
-      digitalWrite(pump3, LOW);
-    }
-
-    if (elapsedTime < pump4Time) {
-      digitalWrite(pump4, HIGH);
-    } else {
-      digitalWrite(pump4, LOW);
-    }
+  while (maxPumpTime > elapsedTime && isGlassPresent()) {
+    elapsedTime = millis() - startTime;
+    digitalWrite(pump1, elapsedTime < pump1Dur ? HIGH : LOW);
+    digitalWrite(pump2, elapsedTime < pump2Dur ? HIGH : LOW);
+    digitalWrite(pump3, elapsedTime < pump3Dur ? HIGH : LOW);
+    digitalWrite(pump4, elapsedTime < pump4Dur ? HIGH : LOW);
 
     recentPour = true;
   }
@@ -365,7 +350,7 @@ boolean pumpLiquid(double pump1Time, double pump2Time, double pump3Time, double 
   digitalWrite(pump3, LOW);
   digitalWrite(pump4, LOW);
 
-  return (isGlassPresent() && elapsedTime >= 0.0);
+  return isGlassPresent();
 }
 
 /// @brief Function that sets the blink code and state of table and blinks LEDs
@@ -464,12 +449,13 @@ void setColor(RGBColor color) {
 
 /// @brief Function that given the drink code will set the pump times and LED color
 /// @param drinkCode The numeric code of the drink
-/// @param pump1Time The time to be set for this pump
-/// @param pump2Time The time to be set for this pump
-/// @param pump3Time The time to be set for this pump
-/// @param pump4Time The time to be set for this pump
+/// @param pump1Time The time to be set for pump1
+/// @param pump2Time The time to be set for pump2
+/// @param pump3Time The time to be set for pump3
+/// @param pump4Time The time to be set for pump4
 /// @return True if a valid drink's pump times were calculated, else false 
-boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double &pump3Time, double &pump4Time) {
+boolean getPumpTimes(int drinkCode, double &pump1Time, double &pump2Time, double &pump3Time,
+                     double &pump4Time) {
   boolean validDrink = false;
 
   switch (drinkCode) {
